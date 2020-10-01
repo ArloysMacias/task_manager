cbed30396cff4925b81dd73b9894c39897b0d1da
ArloysMacias <arloys.rojas@atg.se>
2020-10-01 23:12:51 +0200

    Adding default port

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def hello():
     return  "Hello"
 
 if __name__== "__main__":
-    app.run(host=os.environ.get("IP"), port= int(os.environ.get("PORT")),debug=True)
+    app.run(host=os.environ.get("IP"), port= int(os.environ.get("PORT", 5000)),debug=True)
